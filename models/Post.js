4740cefd4847d566e556ef0775c27b0f9d54e411
josezanle <joze44_zero@hotmail.com>
2021-12-14 11:41:22 -0300

    MOdel changes, add Boolenan to post

--- models/Post.js
+++ models/Post.js
@@ -13,6 +13,10 @@ const postSchema = new Schema({
     type: String,
     require: true,
   },
+  favorite: {
+    type: Boolean,
+    default: false
+  }
 });
 
 module.exports = mongoose.model("Post", postSchema);
